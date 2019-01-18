9d3e7a1c15c1fbfad3c102ddc23e397972b89f06
Lyudmila Zinina <lzinina1103@gmail.com>
2019-01-18 16:52:10 +0500

    First commit

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,66 @@
+
+<!doctype html>
+<html lang="en">
+
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Jekyll v3.8.5">
+    <title>Blog Template · Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+    <!-- Custom styles for this template -->
+    <link href="https://fonts.googleapis.com/css?family=Playfair+Display:700,900" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="{{asset('blog.css')}}" rel="stylesheet">
+  </head>
+  
+  <body>
+   
+    <div class="container">
+
+
+      @include('layouts.header')
+
+      @include('layouts.nav')
+
+      @include('layouts.jumbotron')
+
+    </div>
+
+<main role="main" class="container">
+  
+  <div class="row">
+
+    @yield('content')
+
+    @include('layouts.sidebar')
+  
+  </div><!-- /.row -->
+
+</main><!-- /.container -->
+
+    @include('layouts.footer')
+
+</body>
+
+</html>
+
+
+
