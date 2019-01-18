2e4ed55fd8edf37c0276a6ebaa5349f4750954e4
Lyudmila Zinina <lzinina1103@gmail.com>
2019-01-18 18:57:39 +0500

    create.php changed

--- resources/views/Posts/create.blade.php
+++ resources/views/Posts/create.blade.php
@@ -3,10 +3,25 @@
 @section('content')
 
     <div class="col-md-8 blog-main">
+		
+    	<h1>Publish a Post</h1>
 
+    	<hr>
 
-			<h1> Create a Post </h1>
+		<form method="POST" action="/posts">
+		  
+		  <div class="form-group">
+		    <label for="title">Title:</label>
+		    <input type="text" class="form-control" id="title" name="title">
+		  </div>
 
+		  <div class="form-group">
+		    <label for="body">Body</label>
+		    <textarea id="body" name="body" class="form-control"></textarea>
+		  </div>
+		  
+		  <button type="submit" class="btn btn-primary">Publish</button>
 
+		</form>	
 	</div>
 @endsection
